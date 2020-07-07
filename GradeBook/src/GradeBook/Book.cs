752444542dbea9d5b14639711b6d5d6780169275
dylantyates <dyates.texas@gmail.com>
2020-07-07 18:16:44 -0500

    feat(test): add letter grade

--- GradeBook/src/GradeBook/Book.cs
+++ GradeBook/src/GradeBook/Book.cs
@@ -39,6 +39,25 @@ namespace GradeBook
 
             result.Average /= grades.Count;
 
+            switch (result.Average)
+            {
+                case var numericGrade when numericGrade >= 90.0:
+                    result.Letter = 'A';
+                    break;
+                case var numericGrade when numericGrade >= 80.0:
+                    result.Letter = 'B';
+                    break;
+                case var numericGrade when numericGrade >= 70.0:
+                    result.Letter = 'C';
+                    break;
+                case var numericGrade when numericGrade >= 60.0:
+                    result.Letter = 'D';
+                    break;
+                default:
+                    result.Letter = 'F';
+                    break;
+            }
+
             return result;
         }
 
