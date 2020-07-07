af7aa19ae61c7d5fc2ba1453e3e52f611d6462e3
dylantyates <dylan_yates1@homedepot.com>
2020-07-07 16:31:04 -0500

    refactor: re organize project structure

---
+++ GradeBook/test/GradeBook.Tests/TypeTests.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace GradeBook.Tests
+{
+    [Fact]
+    public void Test1()
+    {
+        var book1 = GetBook("Book 1");
+        var book2 = GetBook("Book 2");
+
+        Assert.Equal("", book1.Name);
+        Assert.Equal("", book2.Name);
+    }
+}
---
+++ README.md
@@ -0,0 +1,17 @@
+# GradeBook
+
+A grade book built following the C# Fundamentals Course on PluralSight.
+
+## Commands
+
+Run
+
+`dotnet run`
+
+Test
+
+`dotnet test`
+
+Build
+
+`dotnet build`
