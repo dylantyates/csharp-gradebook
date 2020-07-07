d966462e8bdaafc4a0323ddb365e86ec53da49e6
dylantyates <dyates.texas@gmail.com>
2020-07-07 17:06:47 -0500

    fix: trying to fix vscode linting

--- GradeBook/test/GradeBook.Tests/TypeTests.cs
+++ GradeBook/test/GradeBook.Tests/TypeTests.cs
@@ -3,13 +3,16 @@ using Xunit;
 
 namespace GradeBook.Tests
 {
-    [Fact]
-    public void Test1()
+    public class TypeTests
     {
-        var book1 = GetBook("Book 1");
-        var book2 = GetBook("Book 2");
+        [Fact]
+        public void Test1()
+        {
+            var book1 = GetBook("Book 1");
+            var book2 = GetBook("Book 2");
 
-        Assert.Equal("", book1.Name);
-        Assert.Equal("", book2.Name);
+            Assert.Equal("", book1.Name);
+            Assert.Equal("", book2.Name);
+        }
     }
 }
